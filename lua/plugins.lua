f114fcfef4cfa145665a312aa1748e9499e9c4cf
MTGH12022021 <loicho0167277@gmail.com>
2023-02-27 23:28:53 +0700

    setup neovim with lua

---
+++ lua/plugins.lua
@@ -0,0 +1,51 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+
+vim.cmd [[packadd packer.nvim]] --
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- theme
+  use 'dracula/vim' -- theme for vim
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-lualine/lualine.nvim' -- Statusline  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  use 'akinsho/nvim-bufferline.lua'
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
+
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  use 'L3MON4D3/LuaSnip'
+  -- Command code
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
+  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  -- seaech file and word
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'norcalli/nvim-colorizer.lua'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+end)
--- settings/airline.vim
+++
@@ -1,7 +0,0 @@
-"configuracion de vim-airline
-let g:airline#extensions#tabline#enabled = 1	"muestra la linea de pestaña en la que estamos buffer
-let g:airline#extensions#tabline#formatter = 'unique_tail'	"muestra solo el nombre del archivo que estamos modificando
-let g:airline_powerline_fonts=1
-let g:airline_theme='sol'
-let g:airline#extensions#hunks#enabled=0
-let g:airline#extensions#hunks#coc_git = 1
